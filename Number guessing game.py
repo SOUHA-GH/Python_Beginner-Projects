272954721aef67c816e3ab4d757261d9e7ea392f
Souha Ghabri <107365513+SOUHA-GH@users.noreply.github.com>
2022-10-16 00:19:47 +0200

    Create Number guessing game.py

---
+++ Number guessing game.py
@@ -0,0 +1,23 @@
+import random
+import math
+lower = int(input("Enter Lower limit:- "))
+upper = int(input("Enter Upper limit:- "))
+x = random.randint(lower, upper)
+print("\n\tYou only have ",round(math.log(upper - lower + 1, 2))," chances to guess the integer! Good luck.\n")
+count = 0
+while count < math.log(upper - lower + 1, 2):
+	count += 1
+	guess = int(input("Guess a number:- "))
+	if x == guess:
+	    if count == 1:
+	        print("Congrats you did it in ",count, " try")	    
+	    else:
+	        print("Congrats you did it in ",count, " tries")
+	    break
+	elif x > guess:
+		print("Wrong! You guessed too small!")
+	elif x < guess:
+		print("Wrong! You Guessed too high!")
+if count >= math.log(upper - lower + 1, 2):
+	print("\nOoups it's not that too, The number is %d" % x)
+	print("\tBetter Luck Next time!")
